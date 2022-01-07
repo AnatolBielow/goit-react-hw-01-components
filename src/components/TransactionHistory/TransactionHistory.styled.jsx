81b1641be6e4c9cb2abe9f29c0d00237b4111468
Anatol Bielow <84202274+AnatolBielow@users.noreply.github.com>
2022-01-07 01:07:23 +0100

    margin

--- src/components/TransactionHistory/TransactionHistory.styled.jsx
+++ src/components/TransactionHistory/TransactionHistory.styled.jsx
@@ -5,10 +5,10 @@ export const TransactionTable = styled.table`
   border-radius: 10px;
   overflow: hidden;
   padding: 5px 20px;
-  margin-bottom: 30px;
   -webkit-box-shadow: 2px 3px 14px 2px rgba(0, 0, 0, 0.41);
   box-shadow: 2px 3px 14px 2px rgba(0, 0, 0, 0.41);
   margin: 0 auto;
+  margin-bottom: 30px;
   * {
     width: 200px;
     padding: 20px;
